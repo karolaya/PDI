e1c35907d4421866888da1e9d66244c97f88e65a
Julián Sibaja <juliansibaja84@gmail.com>
2017-03-25 18:31:54 +0000

    1st point adjustments

--- PS-02/test.py
+++ PS-02/test.py
@@ -8,14 +8,21 @@ pth = '../data/'
 
 # files to be used as samples
 # list *files* holds the names of the test images
-files = os.listdir(pth)
+files = sorted(os.listdir(pth))
 print files
 
 # Usefull function
 def rg(img_path):
     return cv2.imread(pth+img_path, cv2.IMREAD_GRAYSCALE)
 
-def histo_eq(img,name):
+def histo_eq(img):
+    """ Function that runs histogram equalization in a image array
+        Input arguments
+        img: image array
+        Output
+        img: The image with histogram equalization      
+    """
+    
     hist,_ = np.histogram(img,256,[0,256])
     histl = list(hist.flatten())
     
@@ -28,24 +35,17 @@ def histo_eq(img,name):
     N = img.shape[0]
     M = img.shape[1]
     cdf_min = min(cdf)
-    print type(img)
     img = np.array(img)
-    #for x in np.nditer(img, op_flags=['writeonly']):
-    #    x = ((cdf_v.get(int(x))-cdf_min)*(255))/(M*N-1)
     for i in range(0,N):
         for j in range(0,M):
             img[i][j]= ((cdf_v.get(img[i][j])-cdf_min)*(255))/(M*N-1)
-        
-    print(str(N)+"x"+str(M))
-    print img
     return img
             
     
-if __name__ == "__main__" :
-  for i in [2,4,7,8,11,17]:
+for i in [6,9,10,13,15,17]:
     print(files[i])
     img = rg(files[i])
-    imgh = histo_eq(rg(files[i]),files[i])
+    imgh = histo_eq(rg(files[i]))
     mplt.figure()
     mplt.subplot(221)
     mplt.imshow(rg(files[i]), cmap='gray')
@@ -62,9 +62,11 @@ if __name__ == "__main__" :
     mplt.bar(center, hist, align='center', width=width)
     p_cdf_n, = mplt.plot(cdf_n,color = 'r')
     mplt.legend([p_cdf_n], ['cdf'])
+    mplt.xlim( (0, 255) )
     mplt.subplot(224)
     hist, bins = np.histogram(imgh.flatten(),256,[0,256])
     width = 0.7 * (bins[1] - bins[0])
     center = (bins[:-1] + bins[1:]) / 2
     mplt.bar(center, hist, align='center', width=width)
-    mplt.show()        
+    mplt.xlim( (0, 255) )
+    mplt.show(block = False)           
